ace92ddd7520cd2101790af05bf04dad11288459
shiblesadik <shiblesadik@outlook.com>
2020-12-23 16:30:07 +0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig: {
     apiKey: 'AIzaSyDmYl429JF-yuNNv-io7wwNYage36IyUT4',
     authDomain: 'medicines-bazar.firebaseapp.com',
